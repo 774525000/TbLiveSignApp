09aa3074f28796540b61d4258293797ce6c205cc
hehe <774525000@qq.com>
2021-12-24 11:08:42 +0800

    retrofit

--- app/src/main/java/com/yph/app/http/Request.kt
+++ app/src/main/java/com/yph/app/http/Request.kt
@@ -3,18 +3,24 @@ package com.yph.app.http
 import com.yph.app.App
 import com.yph.app.http.service.SignService
 import com.yph.app.model.Sign
+import okhttp3.OkHttpClient
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.Proxy
 
 object Request {
     private val retrofit =
-        Retrofit.Builder().baseUrl(App.baseUrl).addConverterFactory(GsonConverterFactory.create())
+        Retrofit.Builder().client(object : OkHttpClient() {
+            override fun proxy(): Proxy? {
+                return Proxy.NO_PROXY
+            }
+        }).baseUrl(App.baseUrl).addConverterFactory(GsonConverterFactory.create())
             .build()
     private val signService = retrofit.create(SignService::class.java)
 
     fun getSign(callback: Callback<Sign>) {
-        val a = hashMapOf("data" to "{}")
-        signService.getSign(a).enqueue(callback)
+        val params = hashMapOf("data" to "{}")
+        signService.getSign(params).enqueue(callback)
     }
 }
